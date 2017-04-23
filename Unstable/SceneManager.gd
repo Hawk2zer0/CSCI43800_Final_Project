6f85a8a66fbae9a1ad56a7c1294177c04b76a51c
Hawk2zer0 <djantoc@gmail.com>
2017-04-23 15:20:53 -0400

    Basic Scene Switching Commit
    This will be the basis for scene switching. Will need to adjust the way
    that the hit box works.

---
+++ Unstable/SceneManager.gd
@@ -0,0 +1,37 @@
+extends Node
+
+#Keep Track of our current Scene
+var currentScene = null
+
+#Scene Values
+#Main Scene = 1
+#BattleScene Alpha = 2
+#BattleScene Beta = 3
+
+var sceneID = 1
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	currentScene = get_tree().get_root().get_child(get_tree().get_root().get_child_count() -1)
+	
+func setScene(scene,integer):
+	#clean our current Scene
+	currentScene.queue_free()
+	
+	setSceneID(integer)
+	
+	#load our scene file
+	var s = ResourceLoader.load(scene)
+	
+	#create instance of our scene
+	currentScene = s.instance()
+	
+	#add scene to root
+	get_tree().get_root().add_child(currentScene)	
+
+func setSceneID(integer):
+	sceneID = integer
+	
+func getSceneID():
+	return sceneID
